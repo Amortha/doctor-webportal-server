5e940ebea932b8601b6ebd2bbc31e5ab29cd7c56
Amortha kumar shill <amitsharma1998k@gmail.com>
2022-11-29 23:38:20 +0600

    comit

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const cors = require('cors');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require('jsonwebtoken');
+
 const app = express()
 const port = process.env.PORT || 5000;
 
@@ -14,11 +16,29 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: 'UnAuthorized access' });
+  }
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: 'Forbidden access' })
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
+
+
 async function run() {
   try {
     await client.connect();
     const serviceCollection = client.db('doctorsweb_portal').collection('services');
     const bookingCollection = client.db('doctorsweb_portal').collection('bookings');
+    const userCollection = client.db('doctorsweb_portal').collection('users');
 
     // data Storage
     app.get('/service', async (req, res) => {
@@ -27,6 +47,22 @@ async function run() {
       const services = await cursor.toArray();
       res.send(services)
     })
+
+
+    //Admin section
+    app.put('/user/:email', async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options)
+      const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+      res.send({ result, token });
+    })
+
     // slots counting
     app.get('/available', async (req, res) => {
       const date = req.query.date;
@@ -49,11 +85,18 @@ async function run() {
       res.send(services);
     })
 
-    app.get('/booking', async (req, res) => {
-      const patient =req.query.patient
-      const query = {patient:patient};
-      const bookings = await bookingCollection.find(query).toArray();
-      res.send(bookings);
+    app.get('/booking', verifyJWT, async (req, res) => {
+      const patient = req.query.patient;
+      const decodedEmail = req.decoded.email;
+      if(patient === decodedEmail){
+        const query = { patient: patient };
+        const bookings = await bookingCollection.find(query).toArray();
+       return res.send(bookings);
+      }
+else{
+  return res.status(403).send({message: 'forbidden access'})
+}
+
     })
 
 
