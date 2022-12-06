d140832e605968593e4b8a3b049cb1ce39d11523
Amortha kumar shill <amitsharma1998k@gmail.com>
2022-12-06 18:35:19 +0600

    emailsender

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const cors = require('cors');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const jwt = require('jsonwebtoken');
-var nodemailer = require('nodemailer');
-var sgTransport = require('nodemailer-sendgrid-transport');
+const nodemailer = require('nodemailer');
+const sgTransport = require('nodemailer-sendgrid-transport');
 
 const app = express()
 const port = process.env.PORT || 5000;
@@ -34,10 +34,88 @@ function verifyJWT(req, res, next) {
   });
 }
 
+const emailSenderOptions = {
+  auth: {
+    api_key: process.env.EMAIL_SENDER_KEY
+  }
+}
 
-function sendAppointmentEmail(email,name,data,slot){
+const emailClient = nodemailer.createTransport(sgTransport(emailSenderOptions));
+
+function sendAppointmentEmail(booking){
+  const {patient, patientName, treatment, date, slot} = booking;
+
+  const email = {
+    from: process.env.EMAIL_SENDER,
+    to: patient,
+    subject: `Your Appointment for ${treatment} is on ${date} at ${slot} is Confirmed`,
+    text: `Your Appointment for ${treatment} is on ${date} at ${slot} is Confirmed`,
+    html: `
+      <div>
+        <p> Hello ${patientName}, </p>
+        <h3>Your Appointment for ${treatment} is confirmed</h3>
+        <p>Looking forward to seeing you on ${date} at ${slot}.</p>
+        
+        <h3>Our Address</h3>
+        <p>Andor Killa Bandorban</p>
+        <p>Bangladesh</p>
+        <a href="https://web.programming-hero.com/">unsubscribe</a>
+      </div>
+    `
+  };
+
+  emailClient.sendMail(email, function(err, info){
+    if (err ){
+      console.log(err);
+    }
+    else {
+      console.log('Message sent: ', info);
+    }
+});
 
 }
+
+// const emailSenderOptions = {
+//   auth: {
+//     api_key: process.env.EMAIL_SENDER_KEY
+//   }
+// }
+
+// const emailClint = nodemailer.createTransport(sgTransport(emailSenderOptions));
+
+// function sendAppointmentEmail(booking) {
+//   const { patient, patientName, treatment, date, slot } = booking;
+
+//   var email = {
+//     from: process.env.EMAIL_SENDER,
+//     to:patient,
+//     subject: `your Appointment for ${treatment} is on ${date} at ${slot} is Confirmed`,
+//     text: `your Appointment for ${treatment} is on ${date} at ${slot} is Confirmed`,
+//     html: `
+
+//   <div>
+//   <p>Hello ${patientName},</p>
+//  <h3>your Appointment for${treatment} is comfirmed</h3>
+//  <p>Looking forward to seeing you on ${date} at ${slot}.</P>
+
+
+//  <h3>Our Address</h3>
+//  <p>Andor Killa Bandorban</P>
+//  <p>Bangladesh </p>
+//  <a href = "wondrous-mandazi-e22e31.netlify.app">Developer by Amortha</a>
+//   </div>
+//   `
+//   };
+//   emailClint.sendMail(email, function (err, info) {
+//     if (err){
+//       console.log(err)
+//     }
+//     else {
+//       console.log('Message sent: ', info);
+//     }
+//   });
+
+// }
 
 async function run() {
   try {
@@ -154,8 +232,9 @@ async function run() {
         return res.send({ success: false, booking: exists })
       }
       const result = await bookingCollection.insertOne(booking);
-      sendAppointmentEmail(booking.patient,booking.patientName,booking.data,booking.slot);
-      
+      console.log('sending email')
+      sendAppointmentEmail(booking);
+
       return res.send({ success: true, result });
     });
 
@@ -173,7 +252,7 @@ async function run() {
     //delet doctor data
     app.delete('/doctor/:email', verifyJWT, verifyAdmin, async (req, res) => {
       const email = req.params.email;
-      const filter = {email:email};
+      const filter = { email: email };
       const result = await doctorCollection.deleteOne(filter);
       res.send(result);
     })
