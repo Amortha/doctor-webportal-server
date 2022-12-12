3ccc06e86e6c13561818b627feee56616551c388
Amortha kumar shill <amitsharma1998k@gmail.com>
2022-12-12 17:39:28 +0600

    commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const jwt = require('jsonwebtoken');
 var nodemailer = require('nodemailer');
 var sgTransport = require('nodemailer-sendgrid-transport');
@@ -179,7 +179,16 @@ async function run() {
         return res.status(403).send({ message: 'forbidden access' })
       }
 
-    })
+    });
+
+    // payment related
+app.get('/booking/:id',verifyJWT, async(req,res)=>{
+const id = req.params.id;
+const query = {_id: ObjectId(id)};
+const booking = await bookingCollection.findOne(query);
+res.send(booking);
+})
+
 
 
     // data post
